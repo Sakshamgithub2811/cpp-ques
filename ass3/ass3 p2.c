fe2b624bd3c951dd491743797bdca89c4dad18cd
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2022-08-28 17:00:54 +0530

    28-08-2022 | 5:00 P.M.

---
+++ ass3/ass3 p2.c
@@ -0,0 +1,17 @@
+//Write a program to check whether a given number is divisible by 5 or not
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number :");
+    scanf("%d",&a);
+    if(a%5==0)
+    {
+        printf("number is divisible by 5 ");
+    }
+    else
+    {
+        printf("number is not divisible by 5");
+    }
+    return 0;
+}
---
+++ ass3/ass3 p3.c
@@ -0,0 +1,18 @@
+
+//Write a program to check whether a given number is an even number or an odd number
+#include<stdio.h>
+int main()
+{
+     int a;
+     printf("Enter the numeber:");
+     scanf("%d",&a);
+     if(a%2==0)
+     {
+         printf("Number is even ");
+     }
+     else
+     {
+         printf("numebr is odd ");
+     }
+     return 0;
+}
