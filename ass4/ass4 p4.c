8ad2e192ca1351b8577a8e80466b2656243ad3b2
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2022-08-30 14:19:00 +0530

    30-08-2022 |02:18 P.M.

---
+++ ass4/ass4 p4.c
@@ -0,0 +1,12 @@
+// Write a program to print the first 10 odd natural numbers
+int main()
+{
+    int i=1;
+    while(i<=20)
+    {
+       printf("%d\n",i);
+       i=i+2;
+    }
+    return 0;
+
+}
