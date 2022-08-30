01a70156e0886d9915f116fb165e6e65d2ad3f49
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2022-08-30 13:14:12 +0530

    30-08-2022 | 01:14 P.M.

---
+++ ass4/ass4 p1.c
@@ -0,0 +1,12 @@
+//Write a program to print MySirG 5 times on the screen
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    while(i<=4)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}
---
+++ ass4/ass4 p2.c
@@ -0,0 +1,13 @@
+//Write a program to print the first 10 natural numbers.
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    while(i<=9)
+    {
+        printf("%d\n",i+1);
+        i++;
+    }
+    return 0;
+
+}
