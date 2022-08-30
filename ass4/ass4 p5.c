f90edd936bd9c4dafa6c0cb01ac12b50c8d80dd4
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2022-08-30 15:08:01 +0530

    30-08-2022| 03:07 P.M.

---
+++ ass4/ass4 p5.c
@@ -0,0 +1,12 @@
+//Write a program to print the first 10 odd natural numbers in reverse order.
+#include<stdio.h>
+int main()
+{
+    int i=21;
+    while(i>=1)
+    {
+        printf("%d\n",i);
+        i=i-2;
+    }
+    return 0;
+}
