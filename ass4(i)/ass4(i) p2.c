59fa33e3e4afe3d170a25672b64af4fb29323b11
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2022-09-04 02:01:06 +0530

    04-09-2022 | 02:00 A.M.

---
+++ ass4(i)/ass4(i) p2.c
@@ -0,0 +1,12 @@
+//Write a program to print the first 10 natural numbers.
+#include<stdio.h>
+int main()
+{
+    int i,n=1;
+    for (i=0;i<10;i++)
+    {
+        printf("%d\n",n);
+        n++;
+    }
+    return 0;
+}
